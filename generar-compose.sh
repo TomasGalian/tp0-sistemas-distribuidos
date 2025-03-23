4e61bac7fe007fe185b932ca32de89e511b07b2f
TomasGalian <tomiezeg@gmail.com>
2025-03-23 13:06:40 -0300

    Add generete compose script

---
+++ generar-compose.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+echo "Nombre del archivo de salida: $1"
+echo "Cantidad de clientes: $2"
+
+touch $1
+
+cat <<EOF > $1
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    enviorment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+
+EOF
+
+for i in $(seq 1 $2) 
+do
+  cat <<EOF >> $1
+  client$i:
+    container_name: client$i
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$i
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+
+EOF
+done
+
+cat <<EOF >> $1
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+EOF
