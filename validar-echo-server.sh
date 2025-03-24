55f803be5c199c2a7e511748c2d7b3e3e0860deb
TomasGalian <tomiezeg@gmail.com>
2025-03-24 00:42:24 -0300

    Add validation echo server script

---
+++ validar-echo-server.sh
@@ -0,0 +1,11 @@
+NETWORK="tp0_testing_net"
+PORT=12345
+MESSAGE="server testing"
+
+RESPONSE=$(docker run --rm --network=$NETWORK busybox sh -c "echo '$MESSAGE' | nc server $PORT")
+
+if [[ "$RESPONSE" == "$MESSAGE" ]]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
