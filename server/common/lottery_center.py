0232c0cbf2e44ceed4c17df070fd5ddd5e5b0166
TomasGalian <tomiezeg@gmail.com>
2025-03-27 12:19:43 -0300

    Add server logic for multiple process

--- server/common/lottery_center.py
+++ server/common/lottery_center.py
@@ -1,6 +1,8 @@
 import socket
 import logging
 import signal
+import multiprocessing
+
 
 from common.utils import Bet, store_bets, has_won, load_bets
 
@@ -14,13 +16,24 @@ class LotteryCenter:
         self._lottery_socket.listen(listen_backlog)
         self._client_sockets = {}
         self._max_agencies = int(max_agencies)
+        self._manager = multiprocessing.Manager()  # Create a Manager
+        self._winners_by_agency = self._manager.dict()  # Shared dictionary for winners
+
+        self._barrier_bet = multiprocessing.Barrier(int(max_agencies) + 1)  # Para sincronizar recepción de apuestas
+        self._barrier_sorteo = multiprocessing.Barrier(int(max_agencies) + 1)  # Para sincronizar después del sorteo
+
+
+        self._lock_file = multiprocessing.Lock()  # Lock para evitar race conditions en el archivos
+        self._lock_client_sockets = multiprocessing.Lock()
+        self._lock_winners = multiprocessing.Lock()
 
 
     def handler_Sigterm(self, sig, frame):
         logging.debug('action: shutdown | result: in_progress')
-        for _client_sock in self._client_sockets.values():
-            if _client_sock:
-                _client_sock.close()
+        with self._lock_client_sockets:
+            for _client_sock in self._client_sockets.values():
+                if _client_sock:
+                    _client_sock.close()
 
         self._lottery_socket.close()
         logging.info('action: shutdown | result: success')
@@ -37,23 +50,30 @@ class LotteryCenter:
         # Register signal handler
         signal.signal(signal.SIGTERM, self.handler_Sigterm)
 
+        processes = []
+
         # Recive connections and bets
         for i in range(0, self._max_agencies): # Total agencys
             client_sock = self.__accept_new_connection()
             logging.debug('action: accept_connections | result: success')
-            self.__handle_client_connection(client_sock)
+            p = multiprocessing.Process(target=self.__handle_client_connection, args=(client_sock, self._lock_file, self._lock_client_sockets ,self._lock_winners , self._barrier_bet, self._barrier_sorteo))
+            p.start()
+            processes.append(p)
+
 
         # Once all bets are received, the lottery center will start the lottery
-        winners_by_agency = self._get_winners_by_agency()
-        logging.info('action: sorteo | result: success')
-        for agency_id, _client_sock in self._client_sockets.items():
-            action = _client_sock.recv(1)
-            if action == b'\xF0':  # Receive the signal to deliver winners
-                winner_agency = winners_by_agency.get(agency_id, [])
-                self._send_winners(_client_sock, winner_agency)
+        self._barrier_bet.wait()
+        with self._lock_winners:
+            self._winners_by_agency.update(self._get_winners_by_agency())  # Update shared dictionary
+        logging.info('action: sorteo | result: successs')
+        self._barrier_sorteo.wait()
+
+        for p in processes:
+            p.join()
 
     def _get_winners_by_agency(self):
-        bets_list = load_bets()
+        with self._lock_file:
+            bets_list = load_bets()
         winners = {}
         for bet in bets_list:
             if has_won(bet):
@@ -61,6 +81,11 @@ class LotteryCenter:
                     winners[bet.agency] = []
                 winners[bet.agency].append(bet)
         return winners
+
+    def _handle_winners(self, client_sock, winners):
+        for winner in winners:
+            serialized_winner = self._serialize_bet(winner)
+            client_sock.sendall(serialized_winner)
 
     def _send_winners(self, client_sock, winners):        
         bets_serialized = bytearray()
@@ -114,7 +139,7 @@ class LotteryCenter:
         
         return bets
     
-    def __handle_client_connection(self, client_sock):
+    def __handle_client_connection(self, client_sock, lock_file, lock_client_sockets, lock_winners, barrier_bet, barrier_sorteo):
         """
         Read message from a specific client socket and closes the socket
 
@@ -128,8 +153,9 @@ class LotteryCenter:
             action = int.from_bytes(action, byteorder='big')
             while action >= 1:
                 bets = self._receive_bet(client_sock, action)
-                for bet in bets:
-                    store_bets([bet])
+                with lock_file:                
+                    for bet in bets:
+                        store_bets([bet])
                 logging.info(f'action: apuesta_recibida | result: success | cantidad: {action}')
                     
                 logging.debug('action: send_ack | result: in_progress')
@@ -137,9 +163,25 @@ class LotteryCenter:
 
                 action = client_sock.recv(1)
                 action = int.from_bytes(action, byteorder='big')
-            self._client_sockets[bets[0].agency] = client_sock
+            
+            agency_id = bets[0].agency
+            with lock_client_sockets:
+                self._client_sockets[agency_id] = client_sock
+
+            barrier_bet.wait()
+            barrier_sorteo.wait()
+
+            action = client_sock.recv(1)
+            if action == b'\xF0':  # Receive the signal to deliver winners
+                with lock_winners:
+                    winner_agency = self._winners_by_agency.get(agency_id, [])
+                
+                self._send_winners(client_sock, winner_agency)
+
         except OSError as e:
             logging.info(f'action: apuesta_recibida | result: fail | cantidad: {action}')
+        finally:
+            client_sock.close()
 
     def __accept_new_connection(self):
         """
